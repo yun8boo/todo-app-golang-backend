49629c4c90776c1d3ee89b99159432623768f39b
yun8boo <yuma051521@gmail.com>
2021-07-04 17:34:11 +0900

    add firebase admin sdk

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 
+	firebase "firebase.google.com/go"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
@@ -18,8 +23,38 @@ type Todo struct {
 
 var todos []Todo
 
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// config := &firebase.Config{ProjectID: "	golang-todo-app"}
+		app, err := firebase.NewApp(context.Background(), nil)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+		auth, err := app.Auth(context.Background())
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+		authHeader := r.Header.Get("Authorization")
+		idToken := strings.Replace(authHeader, "Bearer", "", 1)
+
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			// JWT が無効なら Handler に進まず別処理
+			fmt.Printf("error verifying ID token: %v\n", err)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("error verifying ID token\n"))
+			return
+		}
+		log.Printf("Verified ID token: %v\n", token)
+		next.ServeHTTP(w, r)
+	}
+}
+
 func main() {
 	fmt.Println("start")
+
 	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
 	allowedHeaders := handlers.AllowedHeaders([]string{"Authorization"})
@@ -30,7 +65,7 @@ func main() {
 	}
 	r := mux.NewRouter()
 	r.Handle("/public", http.HandlerFunc(getPublic)).Methods("GET")
-	r.Handle("/todos", http.HandlerFunc(getTodos)).Methods("GET")
+	r.Handle("/todos", authMiddleware(http.HandlerFunc(getTodos))).Methods("GET")
 	http.ListenAndServe(":8080", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r))
 }
 
